1e1e9d0bf0722dffdca8fb390aa2e50d1a41233d
Baron King <bkkcreative17@gmail.com>
2020-09-30 14:30:51 -0500

    finished hero section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Hero from "../../components/Hero/Hero";
 import "./Home.scss";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
