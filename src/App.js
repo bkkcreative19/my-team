29e9ebfb93187d5eece3f80651369064242a1bd7
Baron King <bkkcreative17@gmail.com>
2020-09-30 14:39:22 -0500

    finished hero section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.scss";
-import NavBar from "./components/NavBar/NavBar";
+
 import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className='App'>
-      <NavBar />
       <Home />
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
+import NavBar from "../../components/NavBar/NavBar";
 import "./Home.scss";
 
 const Home = () => {
   return (
-    <>
+    <div className='home'>
+      <NavBar />
       <Hero />
-    </>
+    </div>
   );
 };
 
