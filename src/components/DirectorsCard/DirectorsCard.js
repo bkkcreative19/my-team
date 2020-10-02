bfacc2b3718473a09a8950be69de39b634138135
Baron King <bkkcreative17@gmail.com>
2020-10-02 18:26:39 -0500

    finished desktop view of about page

--- src/components/DirectorsCard/DirectorsCard.js
+++ src/components/DirectorsCard/DirectorsCard.js
@@ -1,12 +1,15 @@
 import React from "react";
+import PlusLogo from "../PlusLogo";
+import "./DirectorsCard.scss";
 
 const DirectorsCard = () => {
   return (
     <div className='directors__container-card'>
-      <img src='../../assets/images/avatar-nikita.jpg' alt='' />
+      <img src='../../assets/images/avatar-nikita.jpg' alt='aa' />
       <h3>Nikita Marks</h3>
       <p>Founder & CEO</p>
-      <img src='../../assets/images/icon-cross.svg' alt='' />
+      <PlusLogo />
+      <div className='circle'></div>
     </div>
   );
 };
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,10 @@
 import React from "react";
 import AboutHero from "../../components/AboutHero/AboutHero";
+import Clients from "../../components/Clients/Clients";
 import Directors from "../../components/Directors/Directors";
 import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
+import GetStarted from "../../components/GetStarted/GetStarted";
 import "./About.scss";
 
 const About = () => {
@@ -10,6 +13,9 @@ const About = () => {
       <NavBar />
       <AboutHero />
       <Directors />
+      <Clients />
+      <GetStarted />
+      <Footer />
     </div>
   );
 };
