353e7aca41453178fb7961006ae031fcc00206f7
Baron King <bkkcreative17@gmail.com>
2020-10-05 21:25:13 -0500

    fixed bugs

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,29 +6,31 @@ import "./NavBar.scss";
 
 const NavBar = () => {
   return (
-    <nav className='nav'>
-      <div className='nav__container'>
-        <div className='nav__container-logo'>
-          <Link to='/'>
-            <Logo />
-          </Link>
+    <>
+      <nav className='nav'>
+        <div className='nav__container'>
+          <div className='nav__container-logo'>
+            <Link to='/'>
+              <Logo />
+            </Link>
+          </div>
+          <ul className='nav__container-list'>
+            <li>
+              <Link to='/'>home</Link>
+            </li>
+            <li>
+              <Link to='/about'>about</Link>
+            </li>
+          </ul>
+          <div className='nav__container-btn'>
+            <Link to='/contact'>
+              <Button color='#fff' bgColor='#014e56' text='contact us' />
+            </Link>
+          </div>
         </div>
-        <ul className='nav__container-list'>
-          <li>
-            <Link to='/'>home</Link>
-          </li>
-          <li>
-            <Link to='/about'>about</Link>
-          </li>
-        </ul>
-        <div className='nav__container-btn'>
-          <Link to='/contact'>
-            <Button color='#fff' bgColor='#014e56' text='contact us' />
-          </Link>
-        </div>
-      </div>
-      <i class='hamburger fas fa-bars'></i>
-    </nav>
+        <i class='hamburger fas fa-bars'></i>
+      </nav>
+    </>
   );
 };
 
